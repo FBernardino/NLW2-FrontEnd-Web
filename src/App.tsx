47dd94c0f1434f36c5121a1f1a43f340f829f05d
Filipe Bernardino <filipebernardinofaria@gmail.com>
2020-12-15 10:57:13 -0300

    Commit Initial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Routes from './routes';
+import './assets/styles/global.css';
+import './pages/Landing';
+import { Route } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
---
+++ src/pages/TeacherForm/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css';
+
+function TeacherForm(){
+    return (
+        <div id="page-teacher-form" className="container">
+        <PageHeader  title="Que incrível que você quer dar aulas." 
+            description="O primeiro passo, é preencher esse
+            formulário de inscrição."
+        />
+
+        <main>
+            <fieldset>
+                    <legend> Seus Dados  </legend>
+
+                    <div className="input-block">
+                        <label htmlFor="name">Nome completo</label>
+                        <input type="text" id="subject" />
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="avatar">Avatar</label>
+                        <input type="text" id="subject" />
+                    </div>
+
+
+                    <div className="input-block">
+                        <label htmlFor="whatsapp">WhatsApp</label>
+                        <input type="text" id="subject" />
+                    </div>
+
+
+            </fieldset>
+
+
+        </main>
+   </div>
+    )
+}
+
+
+export default TeacherForm;
